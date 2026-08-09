9e9e92859fcff42e2301a8df0db0302d82da3299
Suryansh-Varma <suryanshvarmakakarlapudi@gmail.com>
2026-08-09 21:49:16 +0530

    added: call/put options

--- upstoxservices/getOptions.ts
+++ upstoxservices/getOptions.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 export async function getOptions(instrument_id: string) {
-  const options = await axios.get(
-    `https://api.upstox.com/v2/instruments/options?instrument_id=${instrument_id}`,
+  const response = await axios.get(
+    `https://api.upstox.com/v2/option/chain?instrument_key=${instrument_id}&expiry_date=2026-08-25`,
     {
       headers: {
         Authorization: `Bearer ${process.env.UPSTOX_API_KEY}`,
-        "Content-Type": "application/json",
+        Accept: "application/json",
       },
     },
   );
-  return options.data.instruments;
+  return response.data;
 }
